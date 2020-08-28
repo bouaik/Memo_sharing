e1089172729178e49721bc404fe2333739606ddd
Lhoussaine <bouaik.lhou@gmail.com>
2020-08-28 08:49:45 +0100

    add simple_form gem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-import "bootstrap"
-
 import "../stylesheets/application"
+import "bootstrap"
+import "@fortawesome/fontawesome-free/js/all";
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
