4dfaac688deeec834ce5c106feeff45762ce0869
Lhoussaine <bouaik.lhou@gmail.com>
2020-08-28 21:29:34 +0100

    add user image placeholder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,5 +17,5 @@ import "@fortawesome/fontawesome-free/js/all";
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
