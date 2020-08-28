587fc9e7273d929f4db4370cc515151e23a8772e
Lhoussaine <bouaik.lhou@gmail.com>
2020-08-28 21:11:07 +0100

    add profile and cover images to edit profile user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters               
         devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:cover_image, :profile_image, :email, :password, :password_confirmation, :current_password) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:cover_image, :profile_image, :email, :password, :password_confirmation, :current_password) }
     end
 end
