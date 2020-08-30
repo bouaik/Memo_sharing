dbbd309063050d59b7b05b114230d3a36cad70bb
Lhoussaine <bouaik.lhou@gmail.com>
2020-08-30 09:24:40 +0100

    add all memo creators

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   resources :memos #, except: [:index]
   resource :friendships, only: [:create, :destroy]
 
